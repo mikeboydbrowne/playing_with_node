d4129635da446a744b4ce0836bd9ff5d53ed15c7
Mike Browne <michaelboydbrowne@gmail.com>
2015-05-19 14:47:06 -0400

    Start working on dynamic payments

--- venmo_terminal.rb
+++ venmo_terminal.rb
@@ -23,6 +23,9 @@ def get_user_id
   $id = JSON.parse(Net::HTTP.get(URI("https://api.venmo.com/v1/me?access_token=#{$access_token}")))["data"]["user"]["id"]
 end
 
+def get_friend_list
+end
+
 def about_me
   puts ""
   puts JSON.pretty_generate(JSON.parse(Net::HTTP.get(URI("https://api.venmo.com/v1/me?access_token=#{$access_token}"))))
@@ -35,50 +38,69 @@ def my_friends
   puts ""
 end
 
-def charge(amount, friend)
+def find_friend(name)
+  return id
+end
+
+def charge(amount, id, note)
+  uri = URI("https://api.venmo.com/v1/payments")
+  # friendid = "1621210827849728258"
+  # note = "Test Payment"
+  # amount = -1
+  params = {'access_token' => $access_token, 'user_id' => id, 'note' => note, 'amount' => amount}
+  puts JSON.pretty_generate(JSON.parse(Net::HTTP.post_form(uri,params)))
+end
+
+def pay(amount, friend, note)
   puts amount
   puts friend
 end
 
-def pay(amount, friend)
-  puts amount
-  puts friend
-end
-
-# Get access token for the program
+# Initialize program with access token, user_id, and friend => user_id mappings
 puts ""
 $access_token = ask "Please input your Venmo access token: "
-get_user_id
+get_user_id       # get user_id associated with this access_token
+get_friend_list   # get friend => user_id mappings
 
-# Initial usage message
+# Input loop
 usage(0)
-# Read input/execute commands from the user
 while true do
   input = ask "> "
+
   # exit the program
   if input == "quit"
     break
+
   # Get JSON of user attributes
   elsif input == "about_me"
     about_me
+
   # Get JSON of user's friends
   elsif input == "my_friends"
     my_friends
+
   # Charge a friend
   elsif input == "charge"
     friend = ask "Who do you want to charge?"
+    id = find_friend(friend)
     amount = ask "How much do you want to charge " + friend + "?"
-    charge(amount, friend)
+    note = ask "What would you like your charge to say?"
+    charge(amount, friend, note)
+
   # Pay a friend
   elsif input == "pay"
     friend = ask "Who do you want to pay?"
+    id = find_friend(friend)
     amount = ask "How much do you want to pay " + friend + "?"
-    charge(amount, friend)
+    note = ask "What would you like your payment to say?"
+    pay(amount, friend, note)
+
   # Display commands
   elsif input == "help"
     usage(1)
+
+  # Dealing with unknown inputs
   else
     # puts "echo: " + input
   end
-  # usage(1)
 end
